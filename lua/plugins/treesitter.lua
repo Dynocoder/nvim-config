ec3ea37f3b409aa84e8687d74fee0bd0e0dbca7c
Saurav Prashar <sauravprashar21@gmail.com>
2024-01-10 02:30:37 -0500

    first commit

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,14 @@
+return {
+	"nvim-treesitter/nvim-treesitter", 
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+	--		ensure_installed = { "c", "lua", "vim", "vimdoc", "javascript", "html", "css", "c_sharp" },
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end
+}
+
